2df02b583c3a44096011dab3da207e5bcb746fe2
paulachristiny <paula.christiny.silva@usp.br>
2016-03-20 15:19:42 -0300

    objet avoidance

---
+++ src/object_avoid.cpp
@@ -0,0 +1,33 @@
+#include<ros/ros.h>
+#include<std_msgs/Float32.h>
+#include<geometry_msgs/Twist.h>
+
+ros::Publisher twist_pub;
+geometry_msgs::Twist robotspeed;
+
+void sonarCallback(const std_msgs::Float32ConstPtr &msg){
+    
+    ROS_INFO("Received message, %f", msg->data);
+    
+    if ( msg->data!=0)
+    {
+        robotspeed.linear.x = 0.1;
+        robotspeed.angular.z = 0.35;
+    }
+    else
+    {
+        robotspeed.linear.x = 1;
+        robotspeed.angular.z = 0;
+    }
+    twist_pub.publish(robotspeed);
+}
+
+int main(int argc, char **argv){
+    
+    ros::init(argc, argv, "object_avoidance");
+    ros::NodeHandle nh;
+    twist_pub = nh.advertise<geometry_msgs::Twist>("obstacle/Twist",1);
+    ros::Subscriber sonar_sub = nh.subscribe("/vrep/vehicle/frontSonar", 1, sonarCallback);
+    ros::spin();
+    
+}
